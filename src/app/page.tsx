03ae6cd4b34064cf1cb8363c053b890e64e8c5b5
Kasper Welbers <kasperwelbers@gmail.com>
2023-09-19 07:23:05 +0200

    redeploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Layout from '../components/Layout'
-import { load } from 'outstatic/server'
-import ContentGrid from '../components/ContentGrid'
-import markdownToHtml from '../lib/markdownToHtml'
+import Layout from "../components/Layout";
+import { load } from "outstatic/server";
+import ContentGrid from "../components/ContentGrid";
+import markdownToHtml from "../lib/markdownToHtml";
 
 export default async function Index() {
-  const { content, allPosts, allProjects } = await getData()
+  const { content, allPosts, allProjects } = await getData();
 
   return (
     <Layout>
       <div className="max-w-6xl mx-auto px-5">
-        <section className="mt-16 mb-16 md:mb-12">
+        <section className="mt-14 mb-16 md:mb-12">
           <div
             className="prose lg:prose-2xl home-intro"
             dangerouslySetInnerHTML={{ __html: content }}
@@ -32,38 +32,38 @@ export default async function Index() {
         )}
       </div>
     </Layout>
-  )
+  );
 }
 
 async function getData() {
-  const db = await load()
+  const db = await load();
 
   const page = await db
-    .find({ collection: 'pages', slug: 'home' }, ['content'])
-    .first()
+    .find({ collection: "pages", slug: "home" }, ["content"])
+    .first();
 
-  const content = await markdownToHtml(page.content)
+  const content = await markdownToHtml(page.content);
 
   const allPosts = await db
-    .find({ collection: 'posts' }, [
-      'title',
-      'publishedAt',
-      'slug',
-      'coverImage',
-      'description',
-      'tags'
+    .find({ collection: "posts" }, [
+      "title",
+      "publishedAt",
+      "slug",
+      "coverImage",
+      "description",
+      "tags",
     ])
     .sort({ publishedAt: -1 })
-    .toArray()
+    .toArray();
 
   const allProjects = await db
-    .find({ collection: 'projects' }, ['title', 'slug', 'coverImage'])
+    .find({ collection: "projects" }, ["title", "slug", "coverImage"])
     .sort({ publishedAt: -1 })
-    .toArray()
+    .toArray();
 
   return {
     content,
     allPosts,
-    allProjects
-  }
+    allProjects,
+  };
 }
