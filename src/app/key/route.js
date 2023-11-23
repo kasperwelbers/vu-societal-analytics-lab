3fd3334abad108878ad97e012a1b6d604ab69540
Kasper Welbers <kasperwelbers@gmail.com>
2023-11-23 14:27:46 +0100

    rm key

--- src/app/key/route.js
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-
-export async function GET(req) {
-  const secret = req.nextUrl.searchParams.get("secret");
-  if (secret === process.env.GPTSECRET)
-    return Response.json({ key: process.env.GPTKEY });
-  return Response.json({ error: "wrong secret" });
-}
